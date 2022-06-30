bbc425f6796ad217179745c8192f2f3347dd36fe
Sidney Cardoso <sidneyrsc342@gmail.com>
2022-06-29 22:44:15 -0300

    First commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+export function Home() {
+
+  return (
+    <div className="container">
+    <h1>Presence List</h1>
+     <input type="text" placeholder="name..." />
+     <button type="button">Add</button>
+     </div>
+    )
+  }
+
+// export default Home
