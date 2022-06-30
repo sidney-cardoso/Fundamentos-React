b25e894701ef27a6501e178d9a3f476a2f2b78ce
Sidney Cardoso <sidneyrsc342@gmail.com>
2022-06-30 10:56:10 -0300

    Atualização do projeto

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # Fundamentos React
 
-
 Neste repositório será colocado todo o meu aprendizado de React com a RocketSeat
 
-:construction: Ainda em Desenvolvimento :construction:
+Aqui está o resultado do projeto:
+
+<p align="center">
+<img src="app.png" width="90%" >
+</p>
+
+<h2>Executando o projeto:</h2>
+
+
+-   Para instalar as dependências basta colocar em seu terminal :
+
+```bash
+yarn
+```
+
+-   Para executar o projeto:
+
+```bash
+yarn dev
+```
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,62 @@
+import React, { useState, useEffect } from 'react'
+import { Card } from '../../components/Card'
 import './style.css'
-export function Home() {
 
-  return (
-    <div className="container">
-    <h1>Presence List</h1>
-     <input type="text" placeholder="name..." />
-     <button type="button">Add</button>
-     </div>
+export function Home() {
+    const [studentName, setStudentName] = useState('')
+    const [students, setStudents] = useState([])
+    const [user, setUser] = useState({name: '', avatar: ''})
+
+    const handleAddStudent = () => {
+        const newStudent = {
+            name: studentName,
+            time: new Date().toLocaleTimeString('pt-br', {
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit'
+            })
+        }
+        setStudents(prevState => [...prevState, newStudent])
+    }
+    useEffect(() => {
+      async function fetchData() {
+        const response = await fetch('https://api.github.com/users/sidney-cardoso')
+        const data = await response.json()
+        setUser({
+          name: data.name,
+          avatar: data.avatar_url,
+        })
+      }
+      fetchData()
+    }, [])
+
+    return (
+        <div className="container">
+            <header>
+                <h1>Presence List</h1>
+                <div>
+                  <strong>{user.name}</strong>
+                  <img src={user.avatar} alt="Imagem de perfil" />
+                </div>
+            </header>
+            <input
+                type="text"
+                placeholder="name..."
+                onChange={e => setStudentName(e.target.value)}
+            />
+
+            <button type="button" onClick={handleAddStudent}>
+                Add
+            </button>
+            {students.map(student => (
+                <Card
+                    key={student.time}
+                    name={student.name}
+                    time={student.time}
+                />
+            ))}
+        </div>
     )
-  }
+}
 
 // export default Home
